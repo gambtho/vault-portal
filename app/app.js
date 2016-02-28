c50736ad296f8ddea7079427429b90de85af00d9
Thomas Gamble <thomas_gamble@homedepot.com>
2016-02-27 20:05:27 -0430

    added ui-bootstrap

--- app/app.js
+++ app/app.js
@@ -2,12 +2,13 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.auth',
-  'myApp.version',
+    'ngRoute',
+    'ui.bootstrap',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.auth',
+    'myApp.version',
 ]).
 config(['$routeProvider', '$injector', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/auth'});
+    $routeProvider.otherwise({redirectTo: '/auth'});
 }]);
