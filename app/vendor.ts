f6bdb9a8b98598b618b3d592a1f40237a1e38837
Thomas Gamble <thomas_gamble@homedepot.com>
2016-03-11 21:16:41 -0500

    dependency update

--- app/vendor.ts
+++ app/vendor.ts
@@ -1,8 +1,4 @@
 import 'angular2/platform/browser';
-import 'angular2/platform/common_dom';
 import 'angular2/core';
-import 'angular2/router';
 import 'angular2/http';
-
-
-
+import 'angular2/router';
